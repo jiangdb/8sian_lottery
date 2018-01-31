9f46cad549f05d0ccf9a0d51c479afbb59bc4e06
pzq2017 <499839055@qq.com>
2018-01-31 11:00:23 +0800

    fixed

--- app/Models/LotterySettings.php
+++ app/Models/LotterySettings.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class LotterySettings extends Model
 {
     protected $table = 'lottery_settings';
+
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'winners_count'
+    ];
 }
